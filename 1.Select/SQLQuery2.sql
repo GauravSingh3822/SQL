6658140a6962eeb07ec03d70b29d0601722f8c1b
Gaurav_Singh <166813827+GauravSingh3822@users.noreply.github.com>
2024-09-12 22:33:35 +0530

    Add files via upload

---
+++ 1.Select/SQLQuery2.sql
@@ -0,0 +1,14 @@
+
+
+select * from Employees
+
+
+select firstname,Salary from EmployeeRecords
+
+select FirstName,LastName from Employees
+
+select EmployeeID,CONCAT(firstname,' ',LastName) from Employees
+
+select EmployeeID,CONCAT(firstname,' ',LastName)[Full Name]from Employees
+
+select EmployeeID,CONCAT(firstname,' ',LastName)as[Full Name]from Employees
