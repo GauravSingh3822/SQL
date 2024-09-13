8aaf7797e04f399489dae1bd65d54647aeb25f69
Gaurav_Singh <166813827+GauravSingh3822@users.noreply.github.com>
2024-09-13 22:34:29 +0530

    Add files via upload

---
+++ 5.OrderBy.sql
@@ -0,0 +1,16 @@
+
+
+
+select * from [dbo].[EmployeeRecords]
+order by salary
+
+
+select * from [dbo].[EmployeeRecords]
+order by Salary desc
+
+select * from dbo.Employees
+order by Department asc,Salary desc
+
+
+select * from dbo.Employees
+order by Department,Salary desc
